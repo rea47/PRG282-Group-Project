a6db9fbd74d06c4ef5512816dc680cc23fbb1ba1
Vunene Khoza <600676@student.belgiumcampus.ac.za>
2024-11-10 18:50:02 +0200

    Adding and Retrieving New Student

--- Data Layer/Filehandler.cs
+++ Data Layer/Filehandler.cs
@@ -10,8 +10,34 @@ namespace UpdatedProject.Data_Layer
 {
     internal class Filehandler
     {
-        
-    }
+        private const string FilePath = "students.txt";
 
+        // Add a new student to the file
+        public void AddStudent(string id, string name, int age, string course)
+        {
+            using (StreamWriter writer = new StreamWriter(FilePath, true))
+            {
+                writer.WriteLine($"{id},{name},{age},{course}");
+            }
+        }
+
+        // Retrieve all students from the file
+        public List<Students> GetAllStudents()
+        {
+            var students = new List<Students>();
+            if (File.Exists(FilePath))
+            {
+                foreach (var line in File.ReadLines(FilePath))
+                {
+                    var data = line.Split(',');
+                    students.Add(new Students(data[0], data[1], int.Parse(data[2]), data[3]));
+                }
+            }
+            return students;
+        }
+
+    }
 }
+
+
 
