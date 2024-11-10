11c7b4c782dc47b72482df46ae54f2cd922fbaf5
Charmaine Mkhabela <601395@student.belgiumcampus.ac.za>
2024-11-10 19:22:46 +0200

    l added the student class

--- Business Layer/Logic.cs
+++ Business Layer/Logic.cs
@@ -8,6 +8,23 @@ namespace UpdatedProject.Business_Layer
 {
     internal class Students
     {
-        
+        public string StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+
+        public override string ToString()
+        {
+            return $"{StudentID},{StudentName},{Age},{Course}";
+        }
+
+        public Students(string studentID, string studentName, int age, string course)
+        {
+            this.StudentID = studentID;
+            this.StudentName = studentName;
+            this.Age = age;
+            this.Course = course;
+        }
+
     }
 }
