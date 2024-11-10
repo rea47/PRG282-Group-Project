7ef213d4b30fa41eb67843c626dd4de3ea16969c
Vunene Khoza <600676@student.belgiumcampus.ac.za>
2024-11-10 19:40:44 +0200

    Updating and Deleting Students

--- Data Layer/Filehandler.cs
+++ Data Layer/Filehandler.cs
@@ -10,8 +10,32 @@ namespace UpdatedProject.Data_Layer
 {
     internal class Filehandler
     {
-        
-    }
+        private const string FilePath = "students.txt";
 
+        // Update a student's details in the file
+        public void UpdateStudent(string id, string newName, int newAge, string newCourse)
+        {
+            var lines = File.ReadAllLines(FilePath).ToList();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var data = lines[i].Split(',');
+                if (data[0] == id)
+                {
+                    lines[i] = $"{id},{newName},{newAge},{newCourse}";
+                    break;
+                }
+            }
+            File.WriteAllLines(FilePath, lines);
+        }
+
+        // Delete a student from the file
+        public void DeleteStudent(string id)
+        {
+            var lines = File.ReadAllLines(FilePath).Where(line => !line.StartsWith(id)).ToList();
+            File.WriteAllLines(FilePath, lines);
+        }
+    }
 }
+
+
 
